059194f3f6ce8dd97e2050ca2d9bb94ed9ded51c
Daria_Filippova <daria.s.filippova@gmail.com>
2025-03-19 14:17:01 +0300

    #10th homework

--- pages/auto_pract_form_page.py
+++ pages/auto_pract_form_page.py
@@ -1,6 +1,7 @@
 from pages.base_page import BasePage
 from components.components import WebElement
 
+
 class AutoPractFormPage(BasePage):
     def __init__(self, driver):
         self.base_url = 'https://demoqa.com/automation-practice-form'
@@ -13,6 +14,8 @@ class AutoPractFormPage(BasePage):
         self.btn_submit = WebElement(driver, '#submit')
         self.forma = WebElement(driver, '#userForm')
         self.state_btn = WebElement(driver, 'state', 'id')
-        self.state_ncr = WebElement(driver,  "/html/body/div[2]/div/div/div/div[2]/div[2]/form/div[10]/div[2]/div/div", 'xpath')
+        self.inp_state = WebElement(driver, '#react-select-3-input')
+        #self.state_ncr = WebElement(driver,  "/html/body/div[2]/div/div/div/div[2]/div[2]/form/div[10]/div[2]/div/div", 'xpath')
         self.city_btn = WebElement(driver, 'city', 'id')
-        self.city_delhi = WebElement(driver, "/html/body/div[2]/div/div/div/div[2]/div[2]/form/div[10]", 'xpath')
+        self.inp_city = WebElement(driver, '#react-select-4-input')
+        #self.city_delhi = WebElement(driver, "/html/body/div[2]/div/div/div/div[2]/div[2]/form/div[10]", 'xpath')
--- tests/tests_hw/test_login_form.py
+++ tests/tests_hw/test_login_form.py
@@ -1,14 +1,19 @@
 from pages.auto_pract_form_page import AutoPractFormPage
-
-
+from selenium.webdriver.common.keys import Keys
+import time
 def test_login_form(browser):
     auto_form_page = AutoPractFormPage(browser)
     auto_form_page.visit()
 
+    auto_form_page.state_btn.scroll_to_element()
     auto_form_page.state_btn.find_element()
-    auto_form_page.state_btn.click_force()
-    auto_form_page.state_ncr.click_force()
+    auto_form_page.state_btn.click()
+    auto_form_page.inp_state.send_keys(Keys.PAGE_DOWN)
+    auto_form_page.inp_state.send_keys(Keys.ENTER)
+    time.sleep(2)
 
     auto_form_page.city_btn.find_element()
-    auto_form_page.city_btn.click_force()
-    auto_form_page.city_delhi.click_force()
+    auto_form_page.city_btn.click()
+    auto_form_page.inp_city.send_keys(Keys.PAGE_DOWN)
+    auto_form_page.inp_city.send_keys(Keys.ENTER)
+    time.sleep(2)
